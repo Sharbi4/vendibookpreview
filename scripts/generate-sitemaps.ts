1c435e2fcb2716ca4178ad50a0cd5f97bf522c7b
sharbi <shawnnaharbin@vendibook.com>
2026-05-23 01:10:14 -0700

    fix(seo): correct image namespace in sitemap-listings.xml

--- scripts/generate-sitemaps.ts
+++ scripts/generate-sitemaps.ts
@@ -92,7 +92,7 @@ function buildListingsSitemap(listings: Listing[]): string {
 
   return [
     `<?xml version="1.0" encoding="UTF-8"?>`,
-    `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemaps-image/1.1">`,
+    `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">`,
     ...urls,
     `</urlset>`,
     "",
